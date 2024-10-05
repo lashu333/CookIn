1981b23ba30a6ed6555c1f50dd00a61a5787bef9
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-10-05 16:56:46 +0400

    added view for a recipe

---
+++ CookIn/Views/RecipeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListView.swift
+//  CookIn
+//
+//  Created by Lasha Tavberidze on 05.10.24.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
