1e6316363e34241c0ec72f43948a03cfa41b7276
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-10-05 15:40:25 +0400

    Initial Commit

---
+++ CookIn/CookInApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookInApp.swift
+//  CookIn
+//
+//  Created by Lasha Tavberidze on 05.10.24.
+//
+
+import SwiftUI
+
+@main
+struct CookInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
